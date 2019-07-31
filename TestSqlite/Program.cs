cb6dfb81c0050a4e8590a1158a5f378d8e5aaefd
Bruce <gkm85663@gmail.com>
2019-07-31 11:06:39 +0800

    Finish testing insert and select

--- TestSqlite/Program.cs
+++ TestSqlite/Program.cs
@@ -1,21 +1,102 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
+using Dapper;
+using Newtonsoft.Json;
+using Formatting = System.Xml.Formatting;
 
 namespace TestSqlite
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        public static readonly int NumOfData = 5000;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static void Main(string[] args)
+        {
+            InitSQLiteDb();
+            ShowNumOfData();
+            TestInsert();
+            TestSelect();
+            Console.Read();
+        }
+
+        private static string dbPath = @".\Test.sqlite";
+        private static string cnStr = "data source=" + dbPath;
+
+        private static void ShowNumOfData()
+        {
+            Console.WriteLine("Numbers of data: {0}", NumOfData);
+        }
+
+        private static void InitSQLiteDb()
+        {
+            if (File.Exists(dbPath)) return;
+            using (var cn = new SQLiteConnection(cnStr))
+            {
+                cn.Execute(@"
+                CREATE TABLE Player (
+                    Id VARCHAR(2),
+                    TraceId VARCHAR(50),
+                    CreatedOn DATETIME
+                )");
+            }
+        }
+
+        private static void TestInsert()
+        {
+            Console.Write("Insert:");
+            using (var cn = new SQLiteConnection(cnStr))
+            {
+                cn.Open();
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                cn.Execute("DELETE FROM Player");
+                //參數是用@paramName
+                var insertScript =
+                    "INSERT INTO Player VALUES (@Id, @TraceId, @CreatedOn)";
+
+                using (SQLiteTransaction tran = cn.BeginTransaction())
+                {
+                    for (int i = 1; i <= NumOfData; i++)
+                    {
+                        cn.Execute(insertScript, new Player(i.ToString(), Guid.NewGuid().ToString(), DateTime.Now));
+                    }
+                    tran.Commit();
+                }
+                sw.Stop();
+                Console.WriteLine($"Duration={sw.ElapsedMilliseconds:n0}ms");
+
+                //測試Primary Key
+                //                try
+                //                {
+                //                    //故意塞入錯誤資料
+                //                    cn.Execute(insertScript, Player.TestData[0]);
+                //                    throw new ApplicationException("失敗：未阻止資料重複");
+                //                }
+                //                catch (Exception ex)
+                //                {
+                //                    Console.WriteLine($"測試成功:{ex.Message}");
+                //                }
+            }
+        }
+
+        private static void TestSelect()
+        {
+            Console.Write("Select:");
+            Stopwatch sw = new Stopwatch();
+
+            using (var cn = new SQLiteConnection(cnStr))
+            {
+                cn.Open();
+                sw.Start();
+                var list = cn.Query("SELECT * FROM Player");
+                //                Console.WriteLine(
+                //                    JsonConvert.SerializeObject(list, (Newtonsoft.Json.Formatting)Formatting.Indented));
+            }
+            sw.Stop();
+            Console.Write($"Duration={sw.ElapsedMilliseconds:n0}ms");
         }
     }
 }
---
+++ TestSqlite/TestData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace TestSqlite
+{
+    internal class Player
+    {
+        public string Id { get; set; }
+        public string TraceId { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public Player(string id, string traceId, DateTime createdOn)
+        {
+            Id = id;
+            TraceId = traceId;
+            CreatedOn = createdOn;
+        }
+
+        public static Player[] TestData = new Player[]
+        {
+            new Player("1", Guid.NewGuid().ToString(), DateTime.Now),
+            new Player("2", Guid.NewGuid().ToString(), DateTime.Now),
+        };
+    }
+}
